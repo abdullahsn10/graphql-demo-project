365207d1ec34e88c70baaeaca015e9cea386ca44
abdullahsn10 <1201952@student.birzeit.edu>
2024-09-04 21:15:22 +0300

    Build models, Setup Alembic migrations tool

---
+++ src/models/blog.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from src.settings.database import Base
+
+
+class Blog(Base):
+    __tablename__ = "blog"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(String)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="blogs")
---
+++ src/models/user.py
@@ -0,0 +1,13 @@
+from src.settings.database import Base
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.orm import relationship
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    blogs = relationship("Blog", back_populates="owner")
